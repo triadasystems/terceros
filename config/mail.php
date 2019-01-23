fb9b0f6f1d508a47e57fcbff1dbee4e2b46b153e
Emilio Velazco Pineda <emilio.velazcop@live.com.mx>
2019-01-23 14:26:44 -0600

    EVP
    Configuración email

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => 'CORPKIOVWMM03.corp.televisa.com.mx',
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 97),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'sysadmin@televisa.com.mx',
+        'name' => 'MAIL_FROM_NAME', 'sysadmin',
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    // 'encryption' => env('MAIL_ENCRYPTION', 'tls'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => 'sysadmin@televisa.com.mx',
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => '',
 
     /*
     |--------------------------------------------------------------------------
