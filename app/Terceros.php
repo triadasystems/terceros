71f557418ca7079c68ea348136bb19ee2ad1a2cc
evp-triadasystem <emilio.velazcop@gmail.com>
2019-02-08 14:01:49 -0600

    EVP
    Corrección

--- app/Terceros.php
+++ app/Terceros.php
@@ -182,8 +182,8 @@ class Terceros extends Model
             DB::raw("DATEDIFF(tcs_external_employees.low_date, CURDATE()) AS d_dif"), 
             DB::raw("CONCAT(tcs_external_employees.name,' ',tcs_external_employees.lastname1,' ',tcs_external_employees.lastname2) AS full_name"), 
             DB::raw("if(tcs_external_employees.badge_number IS NULL, 'S/N',tcs_external_employees.badge_number) AS gafete"), 
-            "tcs_external_employees.authorizing_name AS autorizador", 
-            "tcs_external_employees.responsible_name AS responsable",
+            // "tcs_external_employees.authorizing_name AS autorizador", 
+            // "tcs_external_employees.responsible_name AS responsable",
             "tcs_cat_suppliers.name AS empresa"
         )
         ->join('tcs_cat_suppliers','tcs_external_employees.tcs_externo_proveedor','=','tcs_cat_suppliers.id')
